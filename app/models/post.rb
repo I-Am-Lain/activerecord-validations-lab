e25a0c78cdac6415b21b32cc6dde97ca526db0cb
I-Am-Lain <vetter.william@gmail.com>
2020-10-14 11:33:38 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,8 @@
 class Post < ActiveRecord::Base
-
+    include ActiveModel::Validations
     validates :title, presence: true
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
-
-    validate :title_validator
-
-    def title_validator
-
-        if title && !["Won't Believe", "Secret", "Top", "Guess"].any? { |w| title.include?(w) }
-            errors.add(:title, "uhoh")  
-        end
-    end
-
-    
+    validates_with TitleValidator
 end
--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,5 +1,7 @@
 class TitleValidator < ActiveModel::Validator
     def validate(record)
-        ["Won't Believe", "Secret", "Top", "Guess"].any? { |w| record.title.include?(w) }    
+        if record.title && !["Won't Believe", "Secret", "Top", "Guess"].any? { |w| record.title.include?(w) }
+            record.errors.add(:title, "uhoh")  
+        end
     end
 end
